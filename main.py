e1a2edba9b441da23db36083e587b58bb8a8f3af
sohil60 <sohilskaf123@gmail.com>
2025-07-13 02:44:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,15 +19,13 @@ async def check_ip(update: Update, context: ContextTypes.DEFAULT_TYPE):
         try:
             res = requests.get(service, timeout=5)
             if res.ok:
-                results.append(f"[{service}]
-{res.text}")
+                results.append(f"[{service}] {res.text}")
             else:
                 results.append(f"[{service}] Request failed with status code {res.status_code}")
         except Exception as e:
             results.append(f"[{service}] Error: {str(e)}")
     reply = "\n\n".join(results) if results else "الفحص فشل."
     await update.message.reply_text(reply)
-
 def main() -> None:
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
